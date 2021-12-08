a425bb3d72f8b32bd05ca9267347610040087cde
Jess Bowers <jessbowers@me.com>
2021-12-07 20:39:17 -0500

    Day 6 pt2

--- lib/2021/6.ex
+++ lib/2021/6.ex
@@ -1,26 +1,42 @@
 import AOC
 
 aoc 2021, 6 do
-  def p1, do: lanternfish() |> spawn(80) |> Enum.count()
-  def p2, do: lanternfish() |> spawn(256) |> Enum.count()
+  def p1, do: lanternfish() |> spawn(80) |> count()
+  def p2, do: lanternfish() |> spawn(256) |> count()
 
   defp lanternfish,
-    do: input_string() |> String.split(",", trim: true) |> Enum.map(&String.to_integer/1)
+    do:
+      input_string()
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sort()
+      |> Enum.frequencies()
 
   defp spawn(fish, days) do
+    # IO.puts("Day #{days}: #{inspect(fish)}")
     if days > 0 do
-      babies = fish |> Enum.filter(&(&1 == 0)) |> Enum.map(&(&1 + 8))
+      baby_count = Map.get(fish, 0, 0)
 
       fish =
-        Enum.map(fish, fn
-          day when day > 0 -> day - 1
-          day when day == 0 -> 6
-        end) ++
-          babies
+        fish
+        |> Map.delete(0)
+        |> Map.to_list()
+        |> Enum.map(fn {day, count} -> {day - 1, count} end)
+        |> Map.new()
+        |> Map.put(8, baby_count)
+        |> Map.get_and_update(6, fn
+          nil -> {nil, baby_count}
+          c -> {c, c + baby_count}
+        end)
+        |> elem(1)
 
       spawn(fish, days - 1)
     else
       fish
     end
+  end
+
+  defp count(fish) do
+    fish |> Map.values() |> Enum.sum()
   end
 end
